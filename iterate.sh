7f275248262ba54df12a1e5029ed43330696b715
Kate Lynch <katelynch@princeton.edu>
2021-03-05 09:37:39 -0500

    Allow the user to supply the manifest at the command line

--- iterate.sh
+++ iterate.sh
@@ -1,8 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 
-for i in $(cat manifest); do
-    command="$(./orient_image.sh "$i")"
-    echo "working on $i"
-    ./orient_image.sh "$i"
-    echo "done on $i"
-done
+MANIFEST=$1
+
+if [ -z "$MANIFEST" ]
+then
+      echo "Supply a manifest filename"
+else
+  for i in $(cat $MANIFEST); do
+      command="$(./orient_image.sh "$i")"
+      echo "working on $i"
+      ./orient_image.sh "$i"
+      echo "done on $i"
+  done
+fi
